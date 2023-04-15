5ebc041d53563ede88182a7f9bf579f6ea052bbc
Jluzano <justinluzano@gmail.com>
2023-04-15 01:03:21 -0700

    Creating penguin class

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ canvas.create_image(0, 0, image = img2, anchor="nw")
 #Initializing penguin & position
 posx = 0
 posy = 0
-penguin = Image.open("idle/0.png")
-penguin2 = ImageTk.PhotoImage(penguin)
-sprite = canvas.create_image(posx, posy, anchor='nw', image=penguin2)
 
 #Arrays to store images for penguin sprites
 idle = ["idle/0.png", "idle/1.png", "idle/2.png", "idle/3.png", "idle/4.png", 
@@ -25,6 +22,38 @@ walkE = ["walk_E/0.png", "walk_E/1.png", "walk_E/2.png", "walk_E/3.png"]
 walkS = ["walk_S/0.png", "walk_S/1.png", "walk_S/2.png", "walk_S/3.png"]
 walkW = ["walk_W/0.png", "walk_W/1.png", "walk_W/2.png", "walk_W/3.png"]
 
+#Array for idle animation
+idle_list = []
+#converting the idle array to PhotoImages
+for image in idle:
+    image_obj = Image.open(image)
+    photo_obj = ImageTk.PhotoImage(image_obj)
+    idle_list.append(photo_obj)
+
+#Creating the Penguin class
+class Penguin:
+    def __init__(self, canvas, x, y, sprites):
+        self.canvas = canvas
+        self.sprites = sprites
+        self.sprite_index = 0
+        self.sprite = canvas.create_image(x, y, anchor='nw', image=sprites[0])
+        self.isIdle = TRUE
+
+    def update_sprite(self):
+        if self.isIdle:
+            self.sprite_index += 1
+            self.sprite_index = self.sprite_index % len(idle)
+            self.canvas.itemconfig(self.sprite, image=idle_list[self.sprite_index])
+        else:
+            self.sprite_index += 1
+            self.sprite_index = self.sprite_index % len(self.sprites)
+            self.canvas.itemconfig(self.sprite, image=self.sprites[self.sprite_index])
+
+    def move(self, x, y, sprites):
+        self.canvas.move(self.sprite, x, y)
+        self.sprites = sprites
+        self.update_sprite()
+        self.isIdle = TRUE
 
 def up(event):
     x = 0
